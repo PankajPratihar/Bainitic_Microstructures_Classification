733587bd759a7c753bf949d99688b51126825bd9
Pankaj Pratihar <pankajpratihar8@gmail.com>
2026-08-13 02:22:02 +0530

    Allow deployed frontend in CORS

--- api/main.py
+++ api/main.py
@@ -159,6 +159,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://bainitic-microstructures-classification-1.onrender.com/",
 ]
 
 app.add_middleware(
